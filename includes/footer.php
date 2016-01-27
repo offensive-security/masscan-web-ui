d5f2174a792e0178aed024e40e32e968bb1c201b
orhiee <orhiee@gmail.com>
2016-01-27 21:39:52 +0200

    Update footer.php
    the app didnt work when moved to any folder other than /var/www/html/ like "/var/www/html/mass/"

--- includes/footer.php
+++ includes/footer.php
@@ -9,9 +9,9 @@
         </div>
     </footer>
     <!-- END FOOTER -->
-    <script src="/assets/plugins/jquery-2.1.4.min.js" type="text/javascript"></script>
-    <script src="/assets/plugins/bootstrap-3.3.6-dist/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="/assets/scripts.js"></script>
+    <script src="./assets/plugins/jquery-2.1.4.min.js" type="text/javascript"></script>
+    <script src="./assets/plugins/bootstrap-3.3.6-dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <script src="./assets/scripts.js"></script>
 </body>
 <!-- END BODY -->
 </html>
