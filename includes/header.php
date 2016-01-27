dfd2e7bf12e48456fa44473c2cf58397ba885815
orhiee <orhiee@gmail.com>
2016-01-27 21:38:28 +0200

    Update header.php
    the app didnt work whem moved to any folder other than /var/www/html/ like "/var/www/html/mass/"

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <title>MASSCAN Web Interface</title>
-    <link href="/assets/plugins/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="/assets/style.css" rel="stylesheet" />
+    <link href="./assets/plugins/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/style.css" rel="stylesheet" />
 </head>
 <!-- END HEAD -->
 <!-- BEGIN BODY -->
@@ -15,7 +15,7 @@
     <div class="container-fluid">
         <div class="navbar-header">
             <h1><a href="/">MASSCAN Web Interface</a></h1>
-            <a href="http://www.offensive-security.com" target="_blank" class="offsec-logo pull-right"><img src="/assets/img/offsec.png"/></a>
+            <a href="http://www.offensive-security.com" target="_blank" class="offsec-logo pull-right"><img src="./assets/img/offsec.png"/></a>
         </div>
     </div><!-- /.container-fluid -->
 </nav>
