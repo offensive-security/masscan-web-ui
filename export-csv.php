070f1589570a2c1ac26c359834ab1aa5b82cdc53
Interference Security <interference-security@users.noreply.github.com>
2016-06-22 22:01:07 +0530

    Updated

--- export-csv.php
+++ export-csv.php
@@ -3,15 +3,14 @@ require dirname(__FILE__).'/config.php';
 require dirname(__FILE__).'/includes/functions.php';
 define('EXPORT', true);
 require dirname(__FILE__).'/includes/data_validation.php';
-$export_data="IP Address,Protocol,Port,State,Reason,Reason TTL\n";
+$export_data="IP Address,Port,Service,Protocol,State\n";
 if (!empty($results)):
 	foreach ($results as $res):
 		$export_data .= long2ip($res['ipaddress']) . ",";
-		$export_data .= $res['protocol'] . ",";
 		$export_data .= $res['port_id'] . ",";
+		$export_data .= $res['service'] . ",";
+		$export_data .= $res['protocol'] . ",";
 		$export_data .= $res['state'] . ",";
-		$export_data .= $res['reason'] . ",";
-		$export_data .= $res['reason_ttl'] . ",";
 		$export_data .= "\n";
 	endforeach;
 	$name = strftime('export_%m_%d_%Y.csv');
