1176ad74c78e4a397923cedc8b57f506d6206760
Interference Security <interference-security@users.noreply.github.com>
2016-06-22 20:12:48 +0530

    Updated to work in sub-folders

--- includes/header.php
+++ includes/header.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8" />
     <title>MASSCAN Web Interface</title>
-    <link href="/assets/plugins/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="/assets/style.css" rel="stylesheet" />
+    <link href="./assets/plugins/bootstrap-3.3.6-dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="./assets/style.css" rel="stylesheet" />
 </head>
 <!-- END HEAD -->
 <!-- BEGIN BODY -->
@@ -14,8 +14,8 @@
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container-fluid">
         <div class="navbar-header">
-            <h1><a href="/">MASSCAN Web Interface</a></h1>
-            <a href="http://www.offensive-security.com" target="_blank" class="offsec-logo pull-right"><img src="/assets/img/offsec.png"/></a>
+            <h1><a href="./">MASSCAN Web Interface</a></h1>
+            <a href="http://www.offensive-security.com" target="_blank" class="offsec-logo pull-right"><img src="./assets/img/offsec.png"/></a>
         </div>
     </div><!-- /.container-fluid -->
 </nav>
